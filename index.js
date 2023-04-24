047c9c609608707c5ff85591bb08244877066507
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-24 23:00:48 +0700

    feat: add bundler

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require("fs");
+const glob = require("glob");
+const pkgj = require("./package.json");
+
+const reads = glob
+	.sync("**/*", {
+		cwd: __dirname,
+		ignore: [
+			"**/node_modules/**",
+			"**/boilerplate/**",
+			"**/.git*",
+			"**/yarn.lock",
+			"**/package.json",
+			"**/releases/**",
+			"**/tmp/**",
+			"**/LICENSE",
+			"**/.yarn/**",
+			"**/.github/**",
+			"**/*.{md,ts}",
+		],
+	})
+	//.map((str) => path.resolve(__dirname, str))
+	.filter((str) => ![__filename].includes(path.resolve(__dirname, str)))
+	.forEach((str) => {
+		pkgj.bin[str] = str;
+	});
+
+fs.writeFileSync(
+	path.resolve(__dirname, "package.json"),
+	JSON.stringify(pkgj, null, 2)
+);

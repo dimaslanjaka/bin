5f16107710bd9f8c1651265bd28bfed504863eaa
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-15 02:24:04 +0700

    rename lib to src

---
+++ src/git-purge.js
@@ -0,0 +1,49 @@
+const { spawnAsync } = require('cross-spawn');
+const glob = require('glob');
+const path = require('path');
+const { delay } = require('./utils');
+
+/** @type {string[]} */
+const dirs = [];
+
+const fetchDirs = (pattern = '**/.git') => {
+  const globStream = new glob.Glob(pattern, {
+    withFileTypes: false,
+    cwd: process.cwd(),
+    ignore: ['**/node_modules/**', '**/vendor/**']
+  });
+
+  globStream.stream().on('data', (result) => {
+    const fullPath = path.resolve(process.cwd(), result);
+    const base = path.dirname(fullPath);
+    // push directory when not exist
+    if (!dirs.includes(base)) dirs.push(base);
+    start();
+  });
+};
+
+let running = false;
+async function start() {
+  // skip run when still running
+  if (running) return;
+  while (dirs.length > 0) {
+    // start
+    running = true;
+
+    const cwd = dirs.shift();
+    console.log('pruning reflog', cwd);
+    await spawnAsync('git', ['reflog', 'expire', '--expire=all', '--all'], { cwd, stdio: 'pipe', shell: true }).catch(
+      (e) => console.log('failed prune reflog', e.message)
+    );
+    // git gc --prune=now --aggressive
+
+    // delay 3s
+    delay(3);
+    // stop
+    running = false;
+  }
+}
+
+// script starts here
+
+fetchDirs();
---
+++ src/ps/isWin.js
@@ -0,0 +1,2 @@
+const isWin = process.platform === 'win32';
+module.exports = isWin;

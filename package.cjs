3af19352c132246c5df6e359261e0b409d0abf80
dimaslanjaka <dimaslanjaka@gmail.com>
2026-06-06 22:08:45 +0700

    refactor(package): remove doc header and print tarball tree after pack

--- package.cjs
+++ package.cjs
@@ -1,16 +1,7 @@
-/**
- * packer.js - Automated tarball (tgz) creator for release folder
- *
- * Requirements: npm i -D https://github.com/dimaslanjaka/node-cross-spawn/tarball/private upath fs-extra
- * Source (raw): https://github.com/dimaslanjaka/nodejs-package-types/raw/main/packer.js
- * GitHub:      https://github.com/dimaslanjaka/nodejs-package-types/blob/main/packer.js
- * Update:      curl -L https://github.com/dimaslanjaka/nodejs-package-types/raw/main/packer.js > packer.js
- * Usage:       node packer.js
- * CI Example:  https://github.com/dimaslanjaka/nodejs-package-types/blob/main/.github/workflows/build-release.yml
- *
- * For ESM projects, download as package.cjs:
- *   curl -L https://github.com/dimaslanjaka/nodejs-package-types/raw/main/packer.js -o package.cjs
- *   Invoke-WebRequest -Uri "https://github.com/dimaslanjaka/nodejs-package-types/raw/main/packer.js" -OutFile "package.cjs"
- */
+const path = require('path');
 
-import('./src/node-package-packer-cli.mjs');
+import('./src/node-package-packer-cli.mjs').then(() => {
+  import('./src/print-tarball-tree.mjs').then((lib) => {
+    lib.mainPrintTgzTree(path.resolve(__dirname, 'releases/bin.tgz'));
+  });
+});
